bf4ce21a0107938897be63345fd342529ef331f3
Goupil63 <goupinelli@gmail.com>
2025-09-22 00:06:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 VINTED_URL = os.getenv("VINTED_URL")
 DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 SEEN_FILE = "seen.json"
-RUN_DURATION = 300  # durée du run en secondes (5 minutes)
+RUN_DURATION = 5 * 3600  # durée du run en secondes (5 heures)
 
 if not VINTED_URL:
     raise SystemExit("⚠️ VINTED_URL non configuré dans les Secrets.")
